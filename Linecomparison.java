6a252485ffab59ec25c1062514dbe34d2a9307ae
MrunalNagalwade <mrunalnagalwade187@gmail.com>
2021-03-12 08:03:00 +0530

    [mtrunal]: Calculated equality of two lenght

--- Linecomparison.java
+++ Linecomparison.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
-public class Linecomparison {
+public class Linecomparisonuc2 {
 	public static void main(String args[]) {
 
 	//declaration
-	double X1, X2, Y1, Y2, Lenghtofline;
+	double X1, X2, Y1, Y2, Lenghtofline , Lenghtofline2;
 	Scanner scan = new Scanner(System.in);
 	//I/P FROM USER
 	System.out.println("Enter point of X1");
@@ -17,5 +17,22 @@ public class Linecomparison {
 	
 	Lenghtofline = Math.sqrt( (Math.pow((X2-X1),2)) + (Math.pow((Y2-Y1),2)) );
 	System.out.println(Lenghtofline);
+	
+	System.out.println("Enter point of X1");
+	X1 = scan.nextDouble();
+	System.out.println("Enter point of X2");
+        X2 = scan.nextDouble();
+	System.out.println("Enter point of Y1");
+        Y1 = scan.nextDouble();
+	System.out.println("Enter point of Y2");
+        Y2 = scan.nextDouble();
+	
+	Lenghtofline2 = Math.sqrt( (Math.pow((X2-X1),2)) + (Math.pow((Y2-Y1),2)) );
+	System.out.println(Lenghtofline2);
+	
+	String s1,s2;
+		s1=Double.toString(Lenghtofline);
+		s2=Double.toString(Lenghtofline2);
+	        System.out.println("Equality of two length is: " + s1.equals(s2));
 }
 }
